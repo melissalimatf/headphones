f89a01aeddd27a4fbd139c4d253ef82bb2caa5ad
Melissa <126491644+melissalimatf@users.noreply.github.com>
2023-08-04 17:43:44 -0300

    Add files via upload

---
+++ buy.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <title>Fones de Ouvido</title>
+</head>
+
+<body>
+  <header class="header">
+    <div class="logotipo">
+      <a href="#" class="logo"><i class='bx bx-headphone'></i>Soundbox</a>
+    </div>
+    <nav aria-label="primária">
+      <ul class="categories">
+        <li><a href="#">Visão geral</a></li>
+        <li><a href="#">Teach Specs</a></li>
+        <li><a href="#">Reviews</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+    </nav>
+    <div class="header-icons">
+      <a href="#"><i class='bx bx-cart'></i></a>
+      <div id="menu">
+        <a href="#"><i class='bx bx-menu'></i></a>
+      </div>
+    </div>
+  </header>
+
+</body>
+
+</html>
